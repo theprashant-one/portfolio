850f739e4c6060b36a7e183d71dc76f8e3b6bdf0
Prashant Kumar <prashantdrew23@gmail.com>
2022-11-23 20:29:58 +0530

    ui/ux updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Layout } from "./components/Layout";
 import { NavbarComponent } from "./components/Navbar";
+import { inject } from "@vercel/analytics";
 
 function App() {
+  inject();
+
   return (
     <Layout>
       <NavbarComponent />
